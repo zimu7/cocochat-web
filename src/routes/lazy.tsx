25b091d0416d60666a85c2522d34c931f2c6d7b5
zimu <zimucode@qq.com>
2026-05-25 21:25:00 +0800

    修复一个启动报错。

--- src/routes/lazy.tsx
+++ src/routes/lazy.tsx
@@ -3,16 +3,12 @@ import { FC, ReactNode, Suspense } from "react";
 import Loading from "@/components/Loading";
 
 type Props = {
-  key?: string;
   children?: ReactNode;
 };
 
-const Lazy: FC<Props> = ({ key, children }) => {
+const Lazy: FC<Props> = ({ children }) => {
   return (
-    <Suspense
-      key={key ?? new Date().getTime()}
-      fallback={<Loading fullscreen={true} context="lazy" />}
-    >
+    <Suspense fallback={<Loading fullscreen={true} context="lazy" />}>
       {children}
     </Suspense>
   );
